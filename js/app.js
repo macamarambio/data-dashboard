4bdd021e6f1b3a8323b838248b41eb0289868c23
berenisser <tamara.torres.gatica@gmail.com>
2017-11-19 20:41:49 -0300

    modificando aun

--- js/app.js
+++ js/app.js
@@ -1,6 +1,16 @@
-/*
- * Funcionalidad de tu producto
- */
+var submenu = document.getElementsByClassName('submenu');
+for (var i = 0; i< submenu.length; i++) {
+	submenu[i].addEventListener("click",showMenu);
+}
 
-// Puedes hacer uso de la base de datos a través de la variable `data`
-console.log(data);
+function showMenu() {
+	var listMenu = this.getElementsByClassName("itemList")[0];
+	if (listMenu.classList.contains("hide")){
+		listMenu.classList.remove("hide");
+		listMenu.classList.add("show");
+	} else {
+		listMenu.classList.remove("show");
+		listMenu.classList.add("hide");
+	}
+}
+
