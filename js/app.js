7dd67f9ff5ca04e7a12cff06af4bcb959b065cf9
macamarambio <marambio.macarena@gmail.com>
2017-11-20 17:25:51 -0300

    mejorando el maqueteado y probando funcionalidad desde js al checkbox

--- js/app.js
+++ js/app.js
@@ -1,5 +1,4 @@
-
-
+/*MENU DROPDOWN*/
 var submenu = document.getElementsByClassName('submenu');
 for (var i = 0; i< submenu.length; i++) {
 	submenu[i].addEventListener("click",showMenu);
@@ -16,9 +15,33 @@ function showMenu() {
 	}
 }
 
-
 /*CHECKBOX FUNCIONALIDAD GRAFICO*/
+/*Variables*/
+var checkStudent = document.getElementById('checkbox1');
+var checkTeach = document.getElementById('checkbox2');
+var checkJedi = document.getElementById('checkbox3');
+var container = document.getElementById('containerimg');
+var imgGraphic = document.getElementById('imgGrap');
+/*Solo opción 1 seleccionada*/
+checkStudent.addEventListener('click', function(){
+	if (checkStudent.checked == true) {
+		imgGraphic.classList.add(hide);
+	}
+});
 
+/*Solo opción 2 seleccionada*/
+checkTeach.addEventListener('click', function(){
+	if (checkTeach.checked == true) {
+		alert('estoy seleccionado');
+	}
+});
+
+/*Solo opción 3 seleccionada*/
+checkJedi.addEventListener('click', function(){
+	if (checkJedi.checked == true) {
+		alert('estoy seleccionado');
+	}
+});
 
 console.log(data);
 
